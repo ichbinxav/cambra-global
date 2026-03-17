bea7ebc5a840f315220cf8143c8aa00aa6788f37
Xavi Martínez Contero <94.martinez.x@gmail.com>
2026-03-17 05:31:52 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
-import { useAuth } from '@/lib/AuthContext';
 
 import Landing from '@/pages/Landing';
 import Onboarding from '@/pages/Onboarding';
