c1b0df6a9583e64120a3872a32ff4d0e8c160220
Xavi Martínez Contero <94.martinez.x@gmail.com>
2026-03-18 08:03:33 +0000

    File changes

--- src/components/deals/DealModal.jsx
+++ src/components/deals/DealModal.jsx
@@ -296,7 +296,6 @@ export default function DealModal({ deal, onClose, userDeal, userEmail, onUserDe
           )}
 
         </AnimatePresence>
-        </div>
         </motion.div>
         </motion.div>
         );
