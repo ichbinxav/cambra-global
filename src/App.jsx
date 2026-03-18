b49b3434ab645f4e4cb7f57d6a3eb7f2e47405cf
Xavi Martínez Contero <94.martinez.x@gmail.com>
2026-03-18 04:44:20 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
-import { LanguageProvider } from '@/lib/i18n';
+import { LanguageProvider } from '@/lib/i18n.jsx';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
 import Landing from '@/pages/Landing';
