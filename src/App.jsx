80d254b36dbada01464cd13debb0760e69e5b2fc
Xavi Martínez Contero <94.martinez.x@gmail.com>
2026-03-19 18:40:25 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -114,7 +114,7 @@ const AuthenticatedApp = () => {
         </Route>
 
         <Route element={<AdminRoute><AdminLayout /></AdminRoute>}>
-          <Route path="/admin" element={<AdminOverview />} />
+          <Route path="/admin" element={<Navigate to="/admin/users" replace />} />
           <Route path="/admin/users" element={<AdminUsers />} />
           <Route path="/admin/users/:id" element={<AdminUserDetail />} />
           <Route path="/admin/applications" element={<AdminApplications />} />
